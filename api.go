f59b222209c2776862126d61b102ec764c65374d
NSEcho <xdaemonx@protonmail.ch>
2023-11-01 21:28:28 +0100

    Add LDFLAGS to cgo

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ package r2pipe
 
 // #cgo CFLAGS: -I/usr/local/include/libr
 // #cgo CFLAGS: -I/usr/local/include/libr/sdb
+// #cgo LDFLAGS: -L/usr/local/lib -lr_core
 // #cgo pkg-config: r_core
 // #include <stdio.h>
 // #include <stdlib.h>
